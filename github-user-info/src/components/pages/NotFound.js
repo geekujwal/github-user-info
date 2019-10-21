f1689668318491def740f1766faa5e8a9486917f
beast557 <ujjwalbhai999@gmail.com>
2019-10-21 22:37:00 +0545

    Remove duplicated directory

--- github-user-info/src/components/pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from "react";
-
-class NotFound extends PureComponent {
-  render() {
-    return <div>Page not found</div>;
-  }
-}
-
-export default NotFound;
--- github-user-info/src/components/users/User.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from "react";
-
-class User extends PureComponent {
-  render() {
-    return <div>User</div>;
-  }
-}
-
-export default User;
