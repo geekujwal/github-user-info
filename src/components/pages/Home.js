fc7ceec4d7059651f96410fd0b8f7734ad5b14ff
beast557 <ujjwalbhai999@gmail.com>
2019-10-22 10:06:27 +0545

    Home and About Page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,24 +7,44 @@ class Home extends Component {
     users: [],
     loading: false
   };
-  async componentDidMount() {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+  // async componentDidMount() {
+  //   this.setState({
+  //     loading: true
+  //   });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+  //   );
+  //   this.setState({
+  //     loading: false,
+  //     users: res.data
+  //   });
+  // }
+  clearUsers = e => {
+    this.setState({
+      users: [],
+      loading: false
+    });
+  };
+  searchUsers = async text => {
     this.setState({
       loading: true
     });
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_ID}`
     );
     this.setState({
       loading: false,
-      users: res.data
+      users: res.data.items
     });
-    console.log(res.data);
-  }
+  };
   render() {
     return (
       <Fragment>
-        <Search />
+        <Search
+          searchUsers={this.searchUsers}
+          clearUsers={this.clearUsers}
+          showClear={this.state.users.length > 0 ? true : false}
+        />
         <Users loading={this.state.loading} users={this.state.users} />
       </Fragment>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,19 +1,54 @@
 import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 
 class Search extends Component {
+  state = {
+    text: ""
+  };
+
+  onChange = e =>
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({
+      text: ""
+    });
+  };
   render() {
+    const { clearUsers, showClear } = this.props;
     return (
       <Fragment>
-        <div className=" row input-field col s6">
-          <input value="Alvin" type="text" className="validate" />
-          <label className="active" htmlFor="first_name2">
-            Github Username
-          </label>
-          <button className="waves-effect waves-light btn">Search</button>
-        </div>
+        <form className="form" onSubmit={this.onSubmit}>
+          <div className=" row input-field col s6">
+            <input
+              type="text"
+              name="text"
+              value={this.state.text}
+              onChange={this.onChange}
+              className="validate"
+              placeholder="Search github users..."
+            />
+            <label className="active" htmlFor="first_name2">
+              Github Username
+            </label>
+            <button className="btn btn-dark btn-block">Search</button>
+          </div>
+        </form>
+        {showClear ? (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        ) : null}
       </Fragment>
     );
   }
 }
-
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired
+};
 export default Search;
